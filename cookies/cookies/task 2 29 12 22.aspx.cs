95481bf1d4b36a16333186e69a2fd82e505fa097
Musab-Ghannam <115452464+Musab-Ghannam@users.noreply.github.com>
2022-12-29 07:26:56 -0800

    29 12

---
+++ cookies/cookies/task 2 29 12 22.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace cookies
+{
+    public partial class task_2_29_12_22 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        { string now;
+            DateTime g = DateTime.Now;
+            now = g.ToString();
+            Label1.Text = now;
+            if (Request.Cookies["LOG"] != null)
+            {
+
+                Label1.Style["color"] = Request.Cookies["LOG"]["name"].ToString();
+            }
+
+
+
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            HttpCookie c = new HttpCookie("LOG");
+            c.Values.Add("name", DropDownList1.SelectedValue);
+
+            Response.Cookies.Add(c);
+
+            Label1.Style["color"] = DropDownList1.SelectedValue;
+
+
+
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+           
+
+           
+
+
+        }
+    }
+}
---
+++ cookies/cookies/task1 29 12 2022.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace cookies
+{
+    public partial class task1_29_12_2022 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.Cookies["L"] != null)
+            {
+
+                FNAME.Value = Request.Cookies["L"]["NAME"].ToString();
+
+                pass.Text = Request.Cookies["L"]["pass"].ToString();
+
+
+
+            }
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            HttpCookie C = new HttpCookie("L");
+            C.Values.Add("NAME", FNAME.Value);
+            C.Values.Add("pass", pass.Text);
+            if (CheckBox1.Checked)
+            {
+                C.Expires = DateTime.Now.AddDays(2);
+
+                Response.Cookies.Add(C);
+
+            }
+        }
+    }
+}
